6381ccab019e3724d6e7cadb7d84b8bcf6fe5a00
Zafuzi <7799288+Zafuzi@users.noreply.github.com>
2023-05-25 17:11:51 -0700

    * cleaned out defaults and added coughdrop

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About()
+{
+    return (
+        <main className={"container-1100 container-centered container-padded"}>
+            <h1>EnzymeAI - About</h1>
+            <a href={"/"}>Home</a>
+        </main>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import "coughdrop/coughdrop.css"
 
 const inter = Inter({ subsets: ['latin'] })
 
